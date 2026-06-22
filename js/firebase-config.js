27da435d21f222e836dceea5e99a4f1f13f721ce
hma64 <mouhamedamineyousfi10@gmail.com>
2026-06-22 19:30:42 +0200

    Add files via upload

---
+++ js/firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBworC63_P0Nj6fhzXSixy3J7K39FoolCY",
+  authDomain: "cristian-lay-9f1ae.firebaseapp.com",
+  projectId: "cristian-lay-9f1ae",
+  storageBucket: "cristian-lay-9f1ae.firebasestorage.app",
+  messagingSenderId: "617781875856",
+  appId: "1:617781875856:web:7ba06f4d7c0dc1b71f3415",
+  measurementId: "G-XP7W8H72RW"
+};
